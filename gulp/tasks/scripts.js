05b692edbcc4efeefcf8aefbebb16c3f503df26c
Loredana Suciu <loredana.elena.suciu@gmail.com>
2017-09-19 19:23:42 -0400

    Completed the gulp build task.

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,5 +1,12 @@
+var gulp = require('gulp');
+var webpack = require('webpack');
+
+/**  or we can write that as:
+
 var gulp = require('gulp'),
 webpack = require('webpack');
+
+**/
 
 gulp.task('scripts', ['modernizr'], function(callback) {
   webpack(require('../../webpack.config.js'), function(err, stats) {
